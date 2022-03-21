534cc3e0361e0ef6e40dce35461a09f38ad36c5e
derek_smits <emrb.2708@gmail.com>
2022-03-21 16:58:56 +0200

    Init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+  def set_query(body)
+    @@body = body
+  end
+  def graph_get
+    @@query = "{
+          users {
+            id
+            username
+                }
+              }"
+
+    @@url = "http://0.0.0.0:3000/graphql/"
+    @@header = {
+      "Content-Type": "application/json"
+    }
+    HTTParty.post(
+      @@url,{
+      headers: @@header,
+      body: {"query" => @@query}.to_json}
+    )
+  end
+  def index
+    @users = graph_get
+  end
+end
